c7692105b5a7d0a41fd1cc1119c3855df62b93b7
william-golovlev <williamgolovlev@gmail.com>
2025-09-09 10:38:32 -0700

    te

--- assets/script.js
+++ assets/script.js
@@ -12,7 +12,7 @@ function setRandomVideo() {
   videoElement.play();
 }
 
-// --- Blog Card Script ---
+// -- Blog Card Script --
 const cardImages = [
   "https://images.unsplash.com/photo-1620121692062-f98299824c0d?auto=format&fit=crop&q=80&w=2670&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1549880338-65ddcdfd017b?auto=format&fit=crop&q=80&w=2670&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
