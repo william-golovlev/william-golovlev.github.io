242ee68c6e288ad739843ef4ef5289b6d2823ceb
william-golovlev <williamgolovlev@gmail.com>
2025-09-09 13:13:58 -0700

    x

--- assets/script.js
+++ assets/script.js
@@ -32,4 +32,15 @@ function setRandomCardImage() {
 document.addEventListener("DOMContentLoaded", function () {
   setRandomVideo();
   setRandomCardImage();
+
+  // New: Hamburger menu toggle for mobile
+  const burger = document.querySelector(".navbar-burger");
+  const menu = document.querySelector(".navbar-menu");
+
+  if (burger && menu) {
+    burger.addEventListener("click", () => {
+      burger.classList.toggle("is-active");
+      menu.classList.toggle("is-active");
+    });
+  }
 });
